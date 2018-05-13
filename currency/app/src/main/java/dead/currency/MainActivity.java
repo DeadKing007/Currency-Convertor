bc7fcf07a6d6a98094caf5eaa4277c28b459b00d
Shivam <sv17@iitbbs.ac.in>
2018-05-13 10:18:11 +0530

    Java File almost Complete App completed

--- currency/app/src/main/java/dead/currency/MainActivity.java
+++ currency/app/src/main/java/dead/currency/MainActivity.java
@@ -2,12 +2,59 @@ package dead.currency;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private EditText currency;
+    private TextView results;
+    private Spinner Currency_list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currency=findViewById(R.id.Enter_Currency);
+        results=findViewById(R.id.Result);
+        Currency_list=findViewById(R.id.List);
+        Currency_list.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        Double c=0.0;
+        String s=currency.getText().toString();
+        if (s.isEmpty()) {
+            results.setText("Enter correct Number");
+            return;
+        }
+        else {
+            c= Double.parseDouble(s);
+        }
+        switch (position){
+            case 1:
+                results.setText(" "+String.format("%.2f",c * 67.8));
+                break;
+            case 2:
+                results.setText(" "+String.format("%.2f",c * 0.84));
+                break;
+            default:
+                results.setText("Choose Currency");
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
